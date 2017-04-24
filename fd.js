44fac7c46c1e156b2a0a80197873c851b20000be
김현구 <hyun9.kim@samsung.com>
2017-04-24 17:27:06 +0900

    modifying..

--- fd.js
+++ fd.js
@@ -1,18 +1,36 @@
-var folder = {
+const fd = {
     path : '',
+    isDir : false,
+    init: function(path, isDir) {
+      this.path = path;
+      this.isDir = isDir;
+      return this;
+    },
     getPath : function(){
       return this.path;
+    },
+    getIsDir : function(){
+      return this.isDir;
+    },
+    toString : function(){
+      console.log("path : " + path + ", isDir : " + isDir);
     }
 };
-var selectedFolder;
 
-exports.selectFolder = function(e, id) {
-  let sel = e.target.files[0];
-  if(folder){
-      selectedFolder = Object.create(folder);
-      selectedFolder.path = sel.path;
-  }
-  if(id){
-    document.getElementById("filePath").innerHTML = selectedFolder.getPath();
-  }
+const srcFolder = {};
+const destFolder = {};
+
+exports.selectSrcFolder = function(e, id) {
+  selectFolder(e,id,srcFolder);
 };
+
+exports.selectDestFolder = function(e, id) {
+  selectFolder(e,id,destFolder);
+};
+
+function selectFolder(e, id, obj){
+  if(fd && id){
+      obj = Object.create(fd).init(e.target.files[0].path, true);
+      document.getElementById(id).innerHTML = obj.getPath();
+  }
+}
--- main.js
+++ main.js
@@ -8,7 +8,6 @@ const BrowserWindow = electron.BrowserWindow
 const path = require('path')
 const url = require('url')
 
-
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
@@ -16,6 +15,7 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow.setMenu(null);
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
