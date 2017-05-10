89515bb3be580c397719a8517907eed88336596c
김현구 <hyun9.kim@samsung.com>
2017-05-10 15:44:59 +0900

    update

--- svnInfo.js
+++ svnInfo.js
@@ -1,9 +1,17 @@
-const SvnClient = require('svn-spawn');
+"use strict"
 
-let svnClient = {};
+const SvnClient = require('svn-spawn')
+const svnErrMsgId = "svnErrMsg"
 
-exports.importFileList = function(url, fromRev, toRev, viewId){
-  let revision = fromRev + ":" + toRev;
+const msg_successLoadFileList = "파일리스트를 불러왔습니다. "
+const msg_successLoadFileCnt = "파일갯수 : "
+
+let svnClient = {}
+
+exports.importFileList = importFileList
+
+function importFileList(url, fromRev, toRev, viewId){
+  let revision = fromRev + ":" + toRev
   svnClient = new SvnClient({
       cwd: url
   });
@@ -12,22 +20,23 @@ exports.importFileList = function(url, fromRev, toRev, viewId){
     if(err){
       //let strContents = new Buffer(err.toString());
       //console.log(err.toString());
-      $('#svnErrMsg').text(err);
-      return;
+
+      document.getElementById(svnErrMsgId).innerHTML = err
+      return
     }
     else{
-      $('#svnErrMsg').text("Success!");
-      $('#'+viewId).val("");
+      let dataSplitArr = data.split(/\n/);
+      document.getElementById(svnErrMsgId).innerHTML = msg_successLoadFileList + msg_successLoadFileCnt + (dataSplitArr.length-1);
+      document.getElementById(viewId).value = ""
       if(data && data.length){
         $.each(data.split(/\n/), function(i, line){
           if(line && line.length){
               if(line.lastIndexOf('.') !== -1){
-                  $('#'+viewId).val($('#'+viewId).val() + "\\" + $.trim(line.substring(2, line.length)) + '\n');
+                  document.getElementById(viewId).value = document.getElementById(viewId).value + "\\" + $.trim(line.substring(2, line.length)) + '\n'
               }
           }
         });
       }
     }
   });
-
 }
